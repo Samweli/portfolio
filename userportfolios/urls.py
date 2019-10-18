bf4da4470c804f71e9dfe58bbab61caa4a76740c
Samweli <smwltwesa6@gmail.com>
2019-10-18 05:54:24 +0300

    added static files settings

--- userportfolios/urls.py
+++ userportfolios/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from portfolio import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,4 @@ urlpatterns = [
     path('users/', views.user_list, name="user_list"),
     path('users/update/',
      views.update_profile, name='update_profile')
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
